ee2613ce0fcb297ea3831c7f02f8c5bcf1bfa194
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-07-10 16:41:09 +0800

    Remove unused variable

--- backend/main.go
+++ backend/main.go
@@ -11,13 +11,13 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		w.Write([]byte("Hello World"))
 	})
 	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
 
 	})
 	r.Get("/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
-		idParam := chi.URLParam(r, "id")
+
 	})
 	http.ListenAndServe(":3000", r)
 }
