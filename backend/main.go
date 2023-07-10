65ed2d15731cd0169ad5612906b2f72d4eae85ab
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-07-10 15:33:14 +0800

    Add chi api and remove grpc

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+	r.Get("/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "id")
+	})
+	http.ListenAndServe(":3000", r)
+}
