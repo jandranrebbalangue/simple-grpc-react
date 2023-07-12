fce45d0226b0f140ece9e9f640934d098b6fc77a
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-07-12 17:34:11 +0800

    Change chi to gin framework and add todo endpoints

--- backend/main.go
+++ backend/main.go
@@ -3,21 +3,43 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gin-gonic/gin"
 )
 
+type todo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
+
+var todos = []todo{{ID: "1", Text: "Jujutsu Kaisen"}, {ID: "2", Text: "Engage Kiss"}}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+func postTodos(c *gin.Context) {
+	var newTodo todo
+	if err := c.BindJSON(&newTodo); err != nil {
+		return
+	}
+	todos = append(todos, newTodo)
+	c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func getTodoByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, v := range todos {
+		if v.ID == id {
+			c.IndentedJSON(http.StatusOK, v)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+}
+
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-	r.Get("/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-	http.ListenAndServe(":3000", r)
+	r := gin.Default()
+	r.GET("/todos", getTodos)
+	r.POST("/todos", postTodos)
+	r.GET("/todos/:id", getTodoByID)
+	r.Run(":8080")
 }
