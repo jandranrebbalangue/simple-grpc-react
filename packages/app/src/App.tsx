24cf285d9096174dd0e69053399b571bd0bfbeb3
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-07-13 19:59:36 +0800

    Remove tailwind and basic todo with reducer

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer } from "react";
+import "./App.css";
+import { initialState, todoReducer } from "./reducers/todosReducer";
 
+type TodoProps = {
+  id: number;
+  text: string;
+  done: boolean;
+};
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {state.map((item: TodoProps) => {
+          return (
+            <React.Fragment key={item.id}>
+              <p>
+                {item.id} &nbsp;
+                {item.text}
+              </p>
+              <p>{item.done}</p>
+              <button
+                onClick={() => dispatch({ type: "DELETE", payload: item.id })}
+              >
+                Delete
+              </button>
+            </React.Fragment>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ packages/app/src/reducers/todosReducer.ts
@@ -0,0 +1,34 @@
+type TodoState = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+type AppActionTodo = {
+  type: string;
+  payload: any;
+};
+
+export const initialState = [
+  {
+    id: 1,
+    text: "Jujutsu kaisen",
+  },
+  { id: 2, text: "Zoom 100" },
+  {
+    id: 3,
+    text: "Engage Kiss",
+  },
+  { id: 4, text: "Eminence in shadow" },
+];
+
+export const todoReducer = (state: any, action: AppActionTodo) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { ...action.payload }];
+    case "DELETE":
+      return state.filter((t: TodoState) => t.id !== action.payload);
+
+    default:
+      return [...state];
+  }
+};
