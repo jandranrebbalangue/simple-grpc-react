59da57ff69829ffd81d6ab04da1cdcb22801e576
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-07-17 14:06:40 +0800

    Initial authorization gin middleware

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -36,10 +39,112 @@ func getTodoByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
 }
 
+type login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+var identityKey = "id"
+
+func helloHandler(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+	user, _ := c.Get(identityKey)
+	log.Printf("claims:%#v\n", user)
+	c.JSON(200, gin.H{
+		"userID":    claims[identityKey],
+		"userName":  user.(*User).UserName,
+		"text":      "Hello World",
+		"firstName": user.(*User).FirstName,
+		"lastName":  user.(*User).LastName,
+	})
+}
+
+type User struct {
+	UserName  string
+	FirstName string
+	LastName  string
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/todos", getTodos)
 	r.POST("/todos", postTodos)
 	r.GET("/todos/:id", getTodoByID)
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "test zone",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*User); ok {
+				return jwt.MapClaims{
+					identityKey: v.UserName,
+				}
+			}
+			return jwt.MapClaims{}
+
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &User{
+				UserName:  claims[identityKey].(string),
+				FirstName: claims[identityKey].(string),
+				LastName:  claims[identityKey].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVals login
+			if err := c.ShouldBind(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			userId := loginVals.Username
+			password := loginVals.Password
+			if (userId == "admin" && password == "admin") || (userId == "test" && password == "test") {
+				return &User{
+					UserName:  userId,
+					FirstName: "John",
+					LastName:  "Doe",
+				}, nil
+			}
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*User); ok && v.UserName == "admin" {
+				return true
+			}
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		TokenLookup:   "header: Authorization,query:token,cookie:jwt",
+		TokenHeadName: "Bearer",
+		TimeFunc:      time.Now,
+	})
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+	errInit := authMiddleware.MiddlewareInit()
+	if errInit != nil {
+		log.Fatal("authMiddleware.MiddlewareInit() Error:" + errInit.Error())
+	}
+	r.POST("/login", authMiddleware.LoginHandler)
+	r.NoRoute(authMiddleware.MiddlewareFunc(), func(c *gin.Context) {
+		claims := jwt.ExtractClaims(c)
+		log.Printf("NoRoute claims:%#v\n", claims)
+		c.JSON(404, gin.H{
+			"code": "PAGE NOT FOUND", "message": "Page not found",
+		})
+	})
+	auth := r.Group("/auth")
+	auth.GET("/refresh_token", authMiddleware.RefreshHandler)
+	auth.Use(authMiddleware.MiddlewareFunc())
+	{
+		auth.GET("hello", helloHandler)
+	}
 	r.Run(":8080")
 }
