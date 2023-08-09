94b7336021b8db5d441a0cbdef78c3a2d3c1ac0b
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-08-09 22:56:15 +0800

    Remove unused code

--- packages/app/src/constants.ts
+++ packages/app/src/constants.ts
@@ -1,23 +1,15 @@
 export type TodosProps = {
-  id: string;
-  text: string;
-};
-export interface DataPayload<T> {
-  [key: string]: T;
-}
-export interface DataResponse<T> {
-  data: T;
-  isLoading: boolean;
-  isError: any;
+  id: string
+  text: string
 }
 export const fetcher = (endpoint: string) =>
   fetch(`${import.meta.env.VITE_BASE_URL as string}${endpoint}`, {
-    method: "GET",
-  }).then((res) => res.json());
+    method: "GET"
+  }).then((res) => res.json())
 
 export async function addTodo(body: TodosProps) {
   await fetch(`${import.meta.env.VITE_BASE_URL as string}/todos`, {
     method: "POST",
-    body: JSON.stringify(body),
-  });
+    body: JSON.stringify(body)
+  })
 }
